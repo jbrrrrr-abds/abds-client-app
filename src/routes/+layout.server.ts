7694e42ef9dc69c899d9e1ae3005c6852dd93b05
Jbrrrrr <anderbrods.jbrrrrr@gmail.com>
2024-05-15 02:36:21 -0700

    safety check to make sure data exists before assigning the data property of the returned prismic object

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,8 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ locals: { safeGetSession } }) => {
   const { session, user, prismic } = await safeGetSession()
+  // safety check if prismic doesn't have an object to return (i.e. not logged in)
+  if (!prismic.data) { return }
   const prismicUser = prismic.data[0]
 
 	return {
